072e868040f0195eeba6556a2521abc5dc04a054
OmerCohen993 <omerplace@gmail.com>
2024-01-27 22:08:22 +0200

    add config code and interface

--- src/test/java/config/JokesConfig.java
+++ src/test/java/config/JokesConfig.java
@@ -1,5 +1,26 @@
 package config;
 
-public class JokesConfig {
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.BeforeClass;
 
+
+public class JokesConfig {
+    @BeforeClass
+    public static void setup(){
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(JokesEndpoints.JOKES_URL)
+                .addHeader("x-rapidapi-host",JokesEndpoints.HOST )
+                .addHeader("x-rapidapi-key", JokesEndpoints.KEY)
+                .addFilter(new RequestLoggingFilter()) //for personal use
+                .addFilter(new ResponseLoggingFilter()) //for personal use
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
 }
---
+++ src/test/java/config/JokesEndpoints.java
@@ -0,0 +1,12 @@
+package config;
+
+import io.restassured.http.Headers;
+
+public interface JokesEndpoints {
+    //GENERAL
+    String JOKES_URL = "https://v2.jokeapi.dev/";
+
+//    Headers
+    String HOST = "jokes.p.rapidapi.com";
+    String KEY = "56d7a4653emsh4c19b463b18e6b7p144eb7jsn030e478c59b2";
+}
